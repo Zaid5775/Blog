829d8865ee79000d1d06da526000f18560c23046
zaid5775 <szaid5775@gmail.com>
2024-10-03 18:27:08 +0530

    App.css

--- api/index.js
+++ api/index.js
@@ -114,6 +114,41 @@ app.post('/login', async (req,res) => {
 
 
 
+
+  app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+    let newPath = null;
+    if (req.file) {
+      const {originalname,path} = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path+'.'+ext;
+      fs.renameSync(path, newPath);
+    }
+  
+    const {token} = req.cookies;
+    jwt.verify(token, process.env.SECRET, {}, async (err,info) => {
+      if (err) throw err;
+      const {id,title,summary,content} = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(400).json('you are not the author');
+      }
+      await postDoc.set({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+      await postDoc.save();  
+      res.json(postDoc);
+    });
+  
+  });
+
+
+
+
   app.get('/post', async (req, res) =>{
      res.json(await Post.find()
       
